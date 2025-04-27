77385a6bb03216686d59001bbeb117d6cf7041fc
Ayush Bansal <ayush0960.becse24@chitkara.edu.in>
2025-04-27 18:41:45 +0530

    ver 1.4 (done some changes in items)

--- view_page_of_listed_items/view.js
+++ view_page_of_listed_items/view.js
@@ -1,9 +1,8 @@
-// Example data for items (copied from listings.js)
 const items = [
     {
         id: 1,
         title: "Lost Wallet",
-        description: "Black leather wallet with credit cards inside.",
+        description: "A black leather wallet containing credit cards, a driver's license, and some cash. The wallet has sentimental value as it was a gift. It was last seen near Tesla Block.",
         location: "Tesla Block",
         date: "15 June 2023",
         image: "https://i.pinimg.com/736x/65/36/a4/6536a4fd58a8caccc905733477fcfc0d.jpg"
@@ -11,7 +10,7 @@ const items = [
     {
         id: 2,
         title: "Found iPhone",
-        description: "iPhone 12 with blue case.",
+        description: "An iPhone 12 with a blue case. The phone is locked and has a small scratch on the back. It was found near Square One. If this belongs to you, please provide proof of ownership.",
         location: "Square One",
         date: "10 June 2023",
         image: "https://i.pinimg.com/736x/c2/0f/2a/c20f2a16bd32dbb321041bc2477818e7.jpg"
@@ -19,7 +18,7 @@ const items = [
     {
         id: 3,
         title: "Lost Keys",
-        description: "Set of car and house keys.",
+        description: "A set of car and house keys attached to a red keychain with the initials 'J.D.' engraved. The keys were last seen near the Main Entrance.",
         location: "Main Entrance",
         date: "5 June 2023",
         image: "https://bettystreff.com/wp-content/uploads/2017/07/keys.jpg"
@@ -27,7 +26,7 @@ const items = [
     {
         id: 4,
         title: "Found Watch",
-        description: "Silver wristwatch near the fountain.",
+        description: "A silver wristwatch found near the fountain. The watch has a black leather strap and the brand name 'Seiko' engraved on the back.",
         location: "Fountain",
         date: "28 May 2023",
         image: "https://i.ebayimg.com/images/g/TnIAAOSwqeRlPVxr/s-l400.jpg"
@@ -35,7 +34,7 @@ const items = [
     {
         id: 5,
         title: "Lost Backpack",
-        description: "Blue backpack with a laptop inside.",
+        description: "A blue backpack containing a laptop, notebooks, and a water bottle. The backpack has a small tear on the side pocket. It was last seen at the University Library.",
         location: "University Library",
         date: "20 May 2023",
         image: "https://i.ebayimg.com/images/g/FKAAAOSwonJmgzIQ/s-l400.jpg"
@@ -43,7 +42,7 @@ const items = [
     {
         id: 6,
         title: "Found Sunglasses",
-        description: "Rayban aviator sunglasses.",
+        description: "A pair of Ray-Ban aviator sunglasses found in the parking lot. The sunglasses are in good condition and were found without a case.",
         location: "Parking Lot",
         date: "15 May 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSngiB-L4wa7pS0e94IZCZVrXmRIhNvQP7hTqD-olzwUfl-Lkt_JnfLUpbH_Wf5LXvdvDU&usqp=CAU"
@@ -51,7 +50,7 @@ const items = [
     {
         id: 7,
         title: "Lost Glasses",
-        description: "Pair of black-rimmed glasses.",
+        description: "A pair of black-rimmed prescription glasses. The glasses were last seen in the Alpha Zone. They are essential for reading and daily use.",
         location: "Alpha Zone",
         date: "12 May 2023",
         image: "https://i.ebayimg.com/00/s/MTIwMFgxNjAw/z/eIkAAOSwvy5lGkde/$_12.JPG?set_id=880000500F"
@@ -59,7 +58,7 @@ const items = [
     {
         id: 8,
         title: "Found Umbrella",
-        description: "Blue umbrella found at the bus stop.",
+        description: "A blue umbrella found at the bus stop. The umbrella has a small logo of 'Rainy Days' printed on the handle.",
         location: "Bus Stop",
         date: "10 May 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1LBQLdpth2JrMLn8qERRqygj1eMvaBjNN7A&s"
@@ -67,7 +66,7 @@ const items = [
     {
         id: 9,
         title: "Found Book",
-        description: "Hardcover book titled 'The Great Gatsby' found in the library.",
+        description: "A hardcover book titled 'The Great Gatsby' found in the library. The book has a bookmark with the name 'Emily' written on it.",
         location: "Library",
         date: "8 May 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLJ9Rlz5DpA8MXu16gzvgLUiNiyPQJpbNBOA&s"
@@ -75,7 +74,7 @@ const items = [
     {
         id: 10,
         title: "Lost Headphones",
-        description: "Black wireless headphones.",
+        description: "A pair of black wireless headphones. The headphones were last seen near the boys' hostel. They are essential for work and leisure.",
         location: "Near Boys Hostel",
         date: "5 May 2023",
         image: "https://di2ponv0v5otw.cloudfront.net/posts/2022/09/29/633659ff2fbf1a30e994223a/m_63365a177dfcc28e8bb48156.jpg"
@@ -83,7 +82,7 @@ const items = [
     {
         id: 11,
         title: "Found Ring",
-        description: "Gold ring with a diamond.",
+        description: "A gold ring with a diamond found near the Flaming Block. The ring has an engraving on the inside with the initials 'A.R.'",
         location: "Flaming Block",
         date: "2 May 2023",
         image: "https://shop.kenanddanadesign.com/cdn/shop/products/IMG_1381_7ba77f24-c6f5-4e84-8999-64151428d647.jpg?v=1733934429&width=1946"
@@ -91,7 +90,7 @@ const items = [
     {
         id: 12,
         title: "Lost Phone Charger",
-        description: "Black phone charger found in the library.",
+        description: "A black phone charger found in the library. The charger is compatible with most Android devices and has a small scratch on the plug.",
         location: "Library",
         date: "1 May 2023",
         image: "https://apollo.olx.in/v1/files/u7j5tm2awz9v1-ADVIN/image;s=300x600;q=60"
@@ -99,7 +98,7 @@ const items = [
     {
         id: 13,
         title: "Found Scarf",
-        description: "Red scarf found at the Pythagoras block.",
+        description: "A red scarf found at the Pythagoras Block. The scarf is made of wool and has a small tag with the brand name 'Winter Warmers.'",
         location: "Pythagoras Block",
         date: "30 April 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkp0-RcbdkWP1WuyBrh116DqMQFdsvaCUezg&s"
@@ -107,7 +106,7 @@ const items = [
     {
         id: 14,
         title: "Lost Camera",
-        description: "Digital camera with a black case.",
+        description: "A digital camera with a black case. The camera contains important photos and was last seen in the Omega Zone.",
         location: "Omega Zone",
         date: "28 April 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-LDQ81d2sXwAeAOZxevtOA4vs3aWQAh_dIgscBUiBCCLjOK6V38lQJlWJ7JZvzXAWFEY&usqp=CAU"
@@ -115,7 +114,7 @@ const items = [
     {
         id: 15,
         title: "Found Locket",
-        description: "Silver locket with a heart pendant.",
+        description: "A silver locket with a heart pendant found near Square One. The locket opens to reveal a small photo inside.",
         location: "Square One",
         date: "25 April 2023",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2t18BKLiE0Ix5WGqc6SCfW6xHaLZ_B36QYwDYJOvl4bE7P8UTVltb762rdTIrIiXWS5E&usqp=CAU"
